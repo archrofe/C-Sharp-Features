64b7021bbff34e3a1c90cf667621b7f0b211b782
archrofe <archrofe@gmail.com>
2017-08-31 11:51:06 +1000

    Added NavMesh/Tower Defense

---
+++ Assets/Tower Defense/Scripts/TowerDefense.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace TowerDefense
+{
+    public class TowerDefense : MonoBehaviour
+    {
+        public Transform target;
+
+        private NavMeshAgent nav;
+
+        void Awake()
+        {
+            nav = GetComponent<NavMeshAgent>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // IF target != null
+            if (target != null)
+            {
+                // Set destination to target's position
+                nav.SetDestination(target.position);
+            }
+        }
+    }
+}
