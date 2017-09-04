148219afafb7db226895d36c13feddde5108fd6e
archrofe <archrofe@gmail.com>
2017-09-04 23:03:19 +1000

    Added RayCast

---
+++ Assets/Minesweeper2D/Scripts/RayCast.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour
+{
+    public GameObject laserPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit other;
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out other))
+            {
+                print(other.point);
+                Instantiate(laserPoint, other.point, Quaternion.identity);
+            }
+        }
+    }
+}
