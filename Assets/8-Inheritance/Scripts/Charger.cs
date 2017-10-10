39cf8a360f9ec7194edef94e024d935e36983a02
archrofe <archrofe@gmail.com>
2017-10-10 16:05:43 +1100

    Inheritance

---
+++ Assets/8-Inheritance/Scripts/Charger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Inheritance
+{
+    public class Charger : Enemy
+    {
+        [Header("Charger")]
+        public float impactForce = 10f;
+        public float knockback = 5f;
+
+        private Rigidbody rigid;
+
+        public override void Attack()
+        {
+            // Add force to self
+        }
+
+        void OnCollisionEnter(Collision col)
+        {
+            // If collision hits player
+                // Add impactForce to player
+        }
+    }
+}
---
+++ Assets/8-Inheritance/Scripts/Splodey.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Inheritance
+{
+    public class Splodey : Enemy
+    {
+        [Header("Splodey")]
+        public float splosionRadius = 5f;
+        public float splosionRate = 3f;
+        public float impactForce = 10f;
+        public GameObject splosionParticles;
+
+        private float splosionTimer = 0f;
+
+        public override void Attack()
+        {
+            // Start ignition timer
+            // If splosionTimer > splosionRate
+                // Call Explode()
+        }
+
+        void Explode()
+        {
+            // Perform Physics OverlapSphere with splosionRadius
+                // Loop through all hits
+                    // If player
+                        // Add impact force to rigidbody
+                    
+            // Destroy self
+        }
+    }
+}
